e961a5e10c29bac3c0b11b1930fb30bfe4405d78
ImbaMDT <45270806+ImbaMDT@users.noreply.github.com>
2025-11-14 16:26:59 +0100

    Delete Energie/scripts directory

--- Energie/scripts/37229.ips.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-//Dokumentation: https://www.symcon.de/de/service/dokumentation/grundlagen/automationen/php-skripte/aktionsskripte/
-SetValue($_IPS['VARIABLE'], $_IPS['VALUE']);
--- Energie/scripts/Beispielskript.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Beispielskript
-echo "Hallo von der SymBox!";
